35d107d7f6bdc76247b7235c6cc986772f2d0081
Marius Ciubotariu <marius.ciubotariu@gmail.com>
2021-02-16 00:52:15 +0000

    improve conversion

--- src/response.rs
+++ src/response.rs
@@ -1,20 +1,25 @@
-
 pub fn from_lambda(
     lambda_response: http::Response<aws_lambda_events::encodings::Body>,
 ) -> hyper::Response<hyper::Body> {
+    // Convert AWS response to hyper response
+    // Convert: status, headers, body
+
+    // make builder and add status
+    let mut builder = hyper::Response::builder().status(lambda_response.status());
+
+    // copy headers
+    let headers = lambda_response.headers().clone();
+    for (key, value) in headers {
+        if let Some(key) = key {
+            builder = builder.header(key, value);
+        }
+    }
+
+    // set body
     let body = match lambda_response.body().clone() {
         aws_lambda_events::encodings::Body::Empty => hyper::Body::empty(),
         aws_lambda_events::encodings::Body::Text(txt) => hyper::Body::from(txt.clone()),
         aws_lambda_events::encodings::Body::Binary(txt) => hyper::Body::from(txt.clone()),
     };
-
-    let mut headers = lambda_response.headers().clone();
-
-    let mut builder = hyper::Response::builder();
-    builder.headers_mut().replace(&mut headers);
-    let builder = builder.status(lambda_response.status());
-    let resp = builder.body(body).expect("Could not build response");
-
-    println!("Response {:?}", resp);
-    resp
+    builder.body(body).expect("Could not build response")
 }
